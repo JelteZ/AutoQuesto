3f65d9c607938a74a997e58a4d0f5349008d1c45
JelteZ <jelte.zagema@gmail.com>
2023-04-14 21:53:33 +0200

    Update Dataprep.py

--- Dataprep.py
+++ Dataprep.py
@@ -13,3 +13,12 @@ lemmatizer = WordNetLemmatizer()
 word = "running"
 lemma = lemmatizer.lemmatize(word, pos='v') # specify part of speech to disambiguate
 print(lemma) # output: run
+
+def tokenize(text):
+    tokens = nltk.word_tokenize(text)
+    return tokens
+
+# Example usage:
+text = "This is a sample sentence."
+tokens = tokenize(text)
+print(tokens)
