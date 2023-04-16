c2ccdf3572669c6f902d0f9fbd0ae48450dfe214
JelteZ <jelte.zagema@gmail.com>
2023-04-16 13:30:09 +0200

    Dataprep, werkend met mappen
    Het dataprep-programma, dat trainingsdata AI-vriendelijker maakt, werkt nu ook door een input- en outputdirectory in te geven.
    Volgende stap is het automatiseren van het creëren van de input-files, aangezien deze aantallen richting de duizenden gaan...

--- Dataprep.py
+++ Dataprep.py
@@ -1,24 +1,57 @@
 import nltk
-from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+import string
+import os
 
-lemmatizer = WordNetLemmatizer()
+# map met de originele tekstbestanden
+input_dir = "C:\Project 1 informatica\Trivauto\origineel"
 
-word = "running"
-lemma = lemmatizer.lemmatize(word, pos='v') # specify part of speech to disambiguate
-print(lemma) # output: runimport nltk
-from nltk.stem import WordNetLemmatizer
+# map waar de opgeschoonde bestanden moeten worden opgeslagen
+output_dir = "C:\Project 1 informatica\Trivauto\prepped"
 
-lemmatizer = WordNetLemmatizer()
+def preprocess_text(text):
 
-word = "running"
-lemma = lemmatizer.lemmatize(word, pos='v') # specify part of speech to disambiguate
-print(lemma) # output: run
+    # Tokenization
+    tokens = word_tokenize(text)
 
-def tokenize(text):
-    tokens = nltk.word_tokenize(text)
-    return tokens
+    # Stopwoordverwijdering
+    stop_words = set(stopwords.words('dutch'))
+    filtered_tokens = [token for token in tokens if token.lower() not in stop_words]
 
-# Example usage:
-text = "This is a sample sentence."
-tokens = tokenize(text)
-print(tokens)
+    # Stemming en lemmatization
+    ps = PorterStemmer()
+    lemmatizer = WordNetLemmatizer()
+    stemmed_tokens = [ps.stem(token) for token in filtered_tokens]
+    lemmatized_tokens = [lemmatizer.lemmatize(token) for token in filtered_tokens]
+
+    # Verwijderen van leestekens en speciale tekens
+    table = str.maketrans('', '', string.punctuation)
+    stripped_tokens = [token.translate(table) for token in lemmatized_tokens]
+
+    # Lowercasing
+    lowercase_tokens = [token.lower() for token in stripped_tokens]
+
+    # Return de gezuiverde tekst
+    return " ".join(lowercase_tokens)
+
+for filename in os.listdir(input_dir):
+    if filename.endswith(".txt"):
+        # pad naar het huidige bestand
+        input_path = os.path.join(input_dir, filename)
+
+        # lees de inhoud van het bestand
+        with open(input_path, "r", encoding="utf-8") as f:
+            text = f.read()
+
+        # voer de preprocessing uit op de tekst
+        prepped_text = preprocess_text(text)
+
+        # bepaal de nieuwe bestandsnaam (met "prepped" erin)
+        output_filename = filename.replace(".txt", "_prepped.txt")
+        output_path = os.path.join(output_dir, output_filename)
+
+        # schrijf de opgeschoonde tekst naar het nieuwe bestand
+        with open(output_path, "w", encoding="utf-8") as f:
+            f.write(prepped_text)
